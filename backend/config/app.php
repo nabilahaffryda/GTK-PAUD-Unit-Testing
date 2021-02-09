d732ea24cf65671ef1df224eb24814bb8c9bbaf6
Azhar Mashuri <azhar@jayantara.co.id>
2021-02-10 06:29:08 +0700

    Tambah konfigurasi untuk url ui

--- backend/config/app.php
+++ backend/config/app.php
@@ -56,6 +56,8 @@ return [
 
     'asset_url' => env('ASSET_URL', null),
 
+    'ui_url' => env('UI_URL', null),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
