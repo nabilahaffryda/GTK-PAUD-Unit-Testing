04c1454391b17a94cc18349038097b819d0e26b5
Azhar Mashuri <azhar@jayantara.co.id>
2021-02-10 05:17:14 +0700

    Tambah model Akun dan Ptk

---
+++ backend/app/Models/Akun.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+/**
+ * App\Models\Akun
+ *
+ * @property string $akun_id
+ * @property null|string $nip
+ * @property string $nama
+ * @property null|string $passwd
+ * @property null|string $tmp_lahir
+ * @property null|Carbon $tgl_lahir
+ * @property null|string $no_telpon
+ * @property null|string $no_hp
+ * @property string $email
+ * @property null|string $kelamin
+ * @property null|string $foto
+ * @property null|int $k_golongan
+ * @property null|int $moodle_id
+ * @property null|int $paspor_id
+ * @property null|string $token
+ * @property null|string $jabatan
+ * @property null|string $instansi_asal
+ * @property null|int $instansi_k_propinsi
+ * @property null|int $instansi_k_kota
+ * @property null|string $instansi_alamat
+ * @property null|string $instansi_kodepos
+ * @property null|string $alamat
+ * @property null|int $k_propinsi
+ * @property null|int $k_kota
+ * @property null|string $kodepos
+ * @property null|string $nik
+ * @property null|string $npwp
+ * @property null|string $rekening_nama
+ * @property null|string $rekening_bank
+ * @property null|string $rekening_cabang
+ * @property null|string $rekening_nomor
+ * @property null|int $k_jabatan_guru
+ * @property null|int $k_jabatan_dosen_ppg
+ * @property null|string $gelar_depan
+ * @property null|string $gelar_belakang
+ * @property string $is_aktif
+ * @property null|string $admin_id
+ *
+ * @method static Builder|Akun whereAkunId($value)
+ * @method static Builder|Akun whereNip($value)
+ * @method static Builder|Akun whereNama($value)
+ * @method static Builder|Akun wherePasswd($value)
+ * @method static Builder|Akun whereTmpLahir($value)
+ * @method static Builder|Akun whereTglLahir($value)
+ * @method static Builder|Akun whereNoTelpon($value)
+ * @method static Builder|Akun whereNoHp($value)
+ * @method static Builder|Akun whereEmail($value)
+ * @method static Builder|Akun whereKelamin($value)
+ * @method static Builder|Akun whereFoto($value)
+ * @method static Builder|Akun whereKGolongan($value)
+ * @method static Builder|Akun whereMoodleId($value)
+ * @method static Builder|Akun wherePasporId($value)
+ * @method static Builder|Akun whereToken($value)
+ * @method static Builder|Akun whereJabatan($value)
+ * @method static Builder|Akun whereInstansiAsal($value)
+ * @method static Builder|Akun whereInstansiKPropinsi($value)
+ * @method static Builder|Akun whereInstansiKKota($value)
+ * @method static Builder|Akun whereInstansiAlamat($value)
+ * @method static Builder|Akun whereInstansiKodepos($value)
+ * @method static Builder|Akun whereAlamat($value)
+ * @method static Builder|Akun whereKPropinsi($value)
+ * @method static Builder|Akun whereKKota($value)
+ * @method static Builder|Akun whereKodepos($value)
+ * @method static Builder|Akun whereNik($value)
+ * @method static Builder|Akun whereNpwp($value)
+ * @method static Builder|Akun whereRekeningNama($value)
+ * @method static Builder|Akun whereRekeningBank($value)
+ * @method static Builder|Akun whereRekeningCabang($value)
+ * @method static Builder|Akun whereRekeningNomor($value)
+ * @method static Builder|Akun whereKJabatanGuru($value)
+ * @method static Builder|Akun whereKJabatanDosenPpg($value)
+ * @method static Builder|Akun whereGelarDepan($value)
+ * @method static Builder|Akun whereGelarBelakang($value)
+ * @method static Builder|Akun whereIsAktif($value)
+ * @method static Builder|Akun whereAdminId($value)
+ */
+class Akun extends Authenticatable
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'akun';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'akun_id';
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'nip'                 => 'string',
+        'nama'                => 'string',
+        'passwd'              => 'string',
+        'tmp_lahir'           => 'string',
+        'no_telpon'           => 'string',
+        'no_hp'               => 'string',
+        'email'               => 'string',
+        'kelamin'             => 'string',
+        'foto'                => 'string',
+        'k_golongan'          => 'int',
+        'moodle_id'           => 'int',
+        'paspor_id'           => 'int',
+        'token'               => 'string',
+        'jabatan'             => 'string',
+        'instansi_asal'       => 'string',
+        'instansi_k_propinsi' => 'int',
+        'instansi_k_kota'     => 'int',
+        'instansi_alamat'     => 'string',
+        'instansi_kodepos'    => 'string',
+        'alamat'              => 'string',
+        'k_propinsi'          => 'int',
+        'k_kota'              => 'int',
+        'kodepos'             => 'string',
+        'nik'                 => 'string',
+        'npwp'                => 'string',
+        'rekening_nama'       => 'string',
+        'rekening_bank'       => 'string',
+        'rekening_cabang'     => 'string',
+        'rekening_nomor'      => 'string',
+        'k_jabatan_guru'      => 'int',
+        'k_jabatan_dosen_ppg' => 'int',
+        'gelar_depan'         => 'string',
+        'gelar_belakang'      => 'string',
+        'is_aktif'            => 'string',
+        'admin_id'            => 'string',
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'nip',
+        'nama',
+        'passwd',
+        'tmp_lahir',
+        'tgl_lahir',
+        'no_telpon',
+        'no_hp',
+        'email',
+        'kelamin',
+        'foto',
+        'k_golongan',
+        'moodle_id',
+        'paspor_id',
+        'token',
+        'jabatan',
+        'instansi_asal',
+        'instansi_k_propinsi',
+        'instansi_k_kota',
+        'instansi_alamat',
+        'instansi_kodepos',
+        'alamat',
+        'k_propinsi',
+        'k_kota',
+        'kodepos',
+        'nik',
+        'npwp',
+        'rekening_nama',
+        'rekening_bank',
+        'rekening_cabang',
+        'rekening_nomor',
+        'k_jabatan_guru',
+        'k_jabatan_dosen_ppg',
+        'gelar_depan',
+        'gelar_belakang',
+        'is_aktif',
+        'admin_id',
+    ];
+}
---
+++ backend/app/Models/Ptk.php
@@ -0,0 +1,236 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+/**
+ * App\Models\Ptk
+ *
+ * @property string $ptk_id
+ * @property null|string $nuptk
+ * @property null|string $nrg
+ * @property int $k_sumber
+ * @property null|string $nama
+ * @property null|string $tmp_lahir
+ * @property null|Carbon $tgl_lahir
+ * @property null|string $kelamin
+ * @property null|string $golongan
+ * @property null|Carbon $tmt_angkat
+ * @property null|int $k_pegawai
+ * @property null|int $k_kualifikasi
+ * @property null|string $jenjang
+ * @property null|int $instansi_id
+ * @property int $sekolah_id
+ * @property null|string $instansi
+ * @property null|string $alamat
+ * @property null|int $k_propinsi
+ * @property null|int $k_kota
+ * @property null|int $k_kecamatan
+ * @property null|int $k_mapel_ukg
+ * @property null|string $passwd
+ * @property null|int $paspor_id
+ * @property null|string $no_hp
+ * @property null|string $email
+ * @property null|string $alt_email
+ * @property null|int $moodle_id
+ * @property null|string $foto
+ * @property null|string $is_kandidat
+ * @property null|string $is_sertifikasi
+ * @property null|int $k_mapel_sertifikasi
+ * @property int $is_verval
+ * @property int $is_setuju_dinas
+ * @property null|string $token_ujian
+ * @property null|float $max_ukg
+ * @property null|string $nip
+ * @property null|int $is_dapodik
+ * @property null|int $k_jenis_ptk
+ * @property null|string $dapodik_ptk_id
+ * @property null|string $dapodik_nama
+ * @property null|int $kebutuhan_khusus_id
+ * @property null|Carbon $wkt_sinkron
+ * @property null|int $is_aktif
+ * @property null|string $akun_id
+ *
+ * @method static Builder|Ptk wherePtkId($value)
+ * @method static Builder|Ptk whereNuptk($value)
+ * @method static Builder|Ptk whereNrg($value)
+ * @method static Builder|Ptk whereKSumber($value)
+ * @method static Builder|Ptk whereNama($value)
+ * @method static Builder|Ptk whereTmpLahir($value)
+ * @method static Builder|Ptk whereTglLahir($value)
+ * @method static Builder|Ptk whereKelamin($value)
+ * @method static Builder|Ptk whereGolongan($value)
+ * @method static Builder|Ptk whereTmtAngkat($value)
+ * @method static Builder|Ptk whereKPegawai($value)
+ * @method static Builder|Ptk whereKKualifikasi($value)
+ * @method static Builder|Ptk whereJenjang($value)
+ * @method static Builder|Ptk whereInstansiId($value)
+ * @method static Builder|Ptk whereSekolahId($value)
+ * @method static Builder|Ptk whereInstansi($value)
+ * @method static Builder|Ptk whereAlamat($value)
+ * @method static Builder|Ptk whereKPropinsi($value)
+ * @method static Builder|Ptk whereKKota($value)
+ * @method static Builder|Ptk whereKKecamatan($value)
+ * @method static Builder|Ptk whereKMapelUkg($value)
+ * @method static Builder|Ptk wherePasswd($value)
+ * @method static Builder|Ptk wherePasporId($value)
+ * @method static Builder|Ptk whereNoHp($value)
+ * @method static Builder|Ptk whereEmail($value)
+ * @method static Builder|Ptk whereAltEmail($value)
+ * @method static Builder|Ptk whereMoodleId($value)
+ * @method static Builder|Ptk whereFoto($value)
+ * @method static Builder|Ptk whereIsKandidat($value)
+ * @method static Builder|Ptk whereIsSertifikasi($value)
+ * @method static Builder|Ptk whereKMapelSertifikasi($value)
+ * @method static Builder|Ptk whereIsVerval($value)
+ * @method static Builder|Ptk whereIsSetujuDinas($value)
+ * @method static Builder|Ptk whereTokenUjian($value)
+ * @method static Builder|Ptk whereMaxUkg($value)
+ * @method static Builder|Ptk whereNip($value)
+ * @method static Builder|Ptk whereIsDapodik($value)
+ * @method static Builder|Ptk whereKJenisPtk($value)
+ * @method static Builder|Ptk whereDapodikPtkId($value)
+ * @method static Builder|Ptk whereDapodikNama($value)
+ * @method static Builder|Ptk whereKebutuhanKhususId($value)
+ * @method static Builder|Ptk whereWktSinkron($value)
+ * @method static Builder|Ptk whereIsAktif($value)
+ * @method static Builder|Ptk whereAkunId($value)
+ */
+class Ptk extends Authenticatable
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'ptk';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'ptk_id';
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'nuptk'               => 'string',
+        'nrg'                 => 'string',
+        'k_sumber'            => 'int',
+        'nama'                => 'string',
+        'tmp_lahir'           => 'string',
+        'kelamin'             => 'string',
+        'golongan'            => 'string',
+        'k_pegawai'           => 'int',
+        'k_kualifikasi'       => 'int',
+        'jenjang'             => 'string',
+        'instansi_id'         => 'int',
+        'sekolah_id'          => 'int',
+        'instansi'            => 'string',
+        'alamat'              => 'string',
+        'k_propinsi'          => 'int',
+        'k_kota'              => 'int',
+        'k_kecamatan'         => 'int',
+        'k_mapel_ukg'         => 'int',
+        'passwd'              => 'string',
+        'paspor_id'           => 'int',
+        'no_hp'               => 'string',
+        'email'               => 'string',
+        'alt_email'           => 'string',
+        'moodle_id'           => 'int',
+        'foto'                => 'string',
+        'is_kandidat'         => 'string',
+        'is_sertifikasi'      => 'string',
+        'k_mapel_sertifikasi' => 'int',
+        'is_verval'           => 'int',
+        'is_setuju_dinas'     => 'int',
+        'token_ujian'         => 'string',
+        'max_ukg'             => 'float',
+        'nip'                 => 'string',
+        'is_dapodik'          => 'int',
+        'k_jenis_ptk'         => 'int',
+        'dapodik_ptk_id'      => 'string',
+        'dapodik_nama'        => 'string',
+        'kebutuhan_khusus_id' => 'int',
+        'is_aktif'            => 'int',
+        'akun_id'             => 'string',
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'nuptk',
+        'nrg',
+        'k_sumber',
+        'nama',
+        'tmp_lahir',
+        'tgl_lahir',
+        'kelamin',
+        'golongan',
+        'tmt_angkat',
+        'k_pegawai',
+        'k_kualifikasi',
+        'jenjang',
+        'instansi_id',
+        'sekolah_id',
+        'instansi',
+        'alamat',
+        'k_propinsi',
+        'k_kota',
+        'k_kecamatan',
+        'k_mapel_ukg',
+        'passwd',
+        'paspor_id',
+        'no_hp',
+        'email',
+        'alt_email',
+        'moodle_id',
+        'foto',
+        'is_kandidat',
+        'is_sertifikasi',
+        'k_mapel_sertifikasi',
+        'is_verval',
+        'is_setuju_dinas',
+        'token_ujian',
+        'max_ukg',
+        'nip',
+        'is_dapodik',
+        'k_jenis_ptk',
+        'dapodik_ptk_id',
+        'dapodik_nama',
+        'kebutuhan_khusus_id',
+        'wkt_sinkron',
+        'is_aktif',
+        'akun_id',
+    ];
+}
