12b36255dbd31abdc1588fc50a30cd3c482ac617
Azhar Mashuri <azhar@jayantara.co.id>
2021-02-10 06:33:41 +0700

    Ganti welcome url dengan info aplikasi

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (\Illuminate\Http\Request $request) {
+    return response()->json([
+        'data' => [
+            'domain' => $request->getHost(),
+            'time'   => \Carbon\Carbon::now()->format('Y-m-d H:i:s'),
+        ],
+    ]);
 });
