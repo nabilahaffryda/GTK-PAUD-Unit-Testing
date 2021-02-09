314b6c133d571f3ba825bca7f2b11cdec310005f
Azhar Mashuri <azhar@jayantara.co.id>
2021-02-10 06:34:49 +0700

    Tambah konfigurasi untuk simpkb

---
+++ backend/config/simpkb.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'url' => env('SIMPKB_URL', 'https://app-dev.simpkb.id'),
+
+    'sertifikat' => [
+        'validasi-url' => env('SERTIFIKAT_VALIDASI_URL', 'https://sertifikat.simpkb.id/%s'),
+    ],
+];
