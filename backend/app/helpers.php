ca3544b127d00858e273ec815ba96dbe97f01910
Azhar Mashuri <azhar@jayantara.co.id>
2021-02-10 06:30:28 +0700

    Tambah helper file

---
+++ backend/app/helpers.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\Akun;
+use App\Models\Instansi;
+use App\Models\Ptk;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * @return Authenticatable|Akun|null
+ */
+function akun()
+{
+    return Auth::guard('web')->user();
+}
+
+function akunId()
+{
+    return akun()?->akun_id;
+}
+
+/**
+ * @return Authenticatable|Ptk|null
+ */
+function ptk()
+{
+    return Auth::guard('ptk')->user();
+}
+
+/**
+ * @return Instansi|null
+ */
+function instansi()
+{
+    return app()->bound('INSTANSI') ? resolve('INSTANSI') : null;
+}
+
+function instansiId()
+{
+    return instansi()?->instansi_id;
+}
