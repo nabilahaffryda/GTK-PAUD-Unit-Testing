ea72fa8f24e26f56390b672ed3033432d43d5005
Kukuh Ary Susanto <kukuh@jayantara.co.id>
2021-04-07 15:07:56 +0700

    Merge modul admin program dan operator

--- frontend/src/router/routes/instansi/admin.js
+++ frontend/src/router/routes/instansi/admin.js
@@ -1,11 +1,19 @@
 import { lazyLoadView } from '@router/helpers';
 export default [
   {
-    path: '/i/:id(\\d+)/admin',
-    name: 'admin',
-    component: () => lazyLoadView(import('@views/instansi/admin/list/Index')),
+    path: '/i/:id(\\d+)/akun/program',
+    name: 'admin-program',
+    component: () => lazyLoadView(import('@views/instansi/admin/list/Program')),
     meta: {
       title: 'Kelola Akun Admin Program',
+    },
+  },
+  {
+    path: '/i/:id(\\d+)/akun/operator',
+    name: 'admin-operator',
+    component: () => lazyLoadView(import('@views/instansi/admin/list/Operator')),
+    meta: {
+      title: 'Kelola Operator LPD',
     },
   },
 ];
---
+++ frontend/src/views/instansi/admin/list/Operator.vue
@@ -0,0 +1,13 @@
+<template>
+  <list-admin jenis="operator" title="Akun Operator LPD" desc="Lorem Ipsum"></list-admin>
+</template>
+
+<script>
+import ListAdmin from './Main';
+export default {
+  name: 'Program',
+  components: { ListAdmin },
+};
+</script>
+
+<style scoped></style>
