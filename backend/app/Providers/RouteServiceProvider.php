4d1f54e51b6194f8dd7c1322f41c747e05ef9178
Azhar Mashuri <azhar@jayantara.co.id>
2021-04-08 14:59:59 +0700

    Ganti middleware untuk jsonapi ke web

--- backend/app/Providers/RouteServiceProvider.php
+++ backend/app/Providers/RouteServiceProvider.php
@@ -43,7 +43,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
-            Route::middleware('api')
+            Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/jsonapi.php'));
 
