4d437c12ae47ab9cf178bec40ff52f8088208210
Kukuh Ary Susanto <kukuh@jayantara.co.id>
2021-04-13 16:05:44 +0700

    Penyesuaian url request modul akses

--- backend/app/Services/AksesService.php
+++ backend/app/Services/AksesService.php
@@ -15,7 +15,7 @@ use Route;
 class AksesService
 {
     protected $namespaces = [
-        'akun' => 'App\\Http\\Controllers\\Instansi',
+        'web' => 'App\\Http\\Controllers\\Instansi',
         'ptk'  => 'App\\Http\\Controllers\\Gtk'
     ];
 
@@ -96,7 +96,7 @@ class AksesService
         return strtolower(Str::snake($controller, '-'));
     }
 
-    public function fromRequest(Request $request, $guard = 'akun')
+    public function fromRequest(Request $request, $guard = 'web')
     {
         $controller = $request->route()->getActionName();
         return $this->fromController($guard, $controller);
