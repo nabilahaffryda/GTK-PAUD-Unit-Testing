3ae8a7007483563b02b73a9dd78c3b9009a62f76
Azhar Mashuri <azhar@jayantara.co.id>
2021-04-13 10:09:21 +0700

    Lengkapi preferensi

--- backend/app/Http/Controllers/Instansi/IndexController.php
+++ backend/app/Http/Controllers/Instansi/IndexController.php
@@ -4,14 +4,34 @@ namespace App\Http\Controllers\Instansi;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\MasterRequest;
+use App\Services\AksesService;
+use App\Services\AkunService;
 use App\Services\MasterService;
 
 class IndexController extends Controller
 {
-    public function preferensi()
+    public function preferensi(AkunService $akunService, AksesService $aksesService)
     {
+        $akun     = akun();
+        $instansi = instansi();
+        $simpkb   = config('simpkb.url');
+
+        $akunInstansi = $akunService->akunInstansis($instansi);
+        $groups       = $akunService->getGroups($akunInstansi);
+        $akses        = $aksesService->getAkses($groups)->pluck('psp_akses_id', 'akses');
+
+        $aktivasi = $akunService->isAktivasi($akunInstansi);
+
         return [
-            ''
+            'akun'     => $akun,
+            'instansi' => $instansi,
+            'groups'   => $groups,
+            'akses'    => $akses,
+
+            'aktivasi' => $aktivasi,
+            'konfig'   => [
+                'simpkb' => $simpkb,
+            ]
         ];
     }
 
