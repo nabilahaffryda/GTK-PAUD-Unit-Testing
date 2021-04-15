b64e9bb48061a9ea331990b24352369d1cec7a8c
Azhar Mashuri <azhar@jayantara.co.id>
2021-04-15 15:14:23 +0700

    Update form request untuk create akun

--- backend/app/Http/Requests/Instansi/Admin/CreateRequest.php
+++ backend/app/Http/Requests/Instansi/Admin/CreateRequest.php
@@ -15,14 +15,16 @@ class CreateRequest extends FormRequest
     {
         return [
             'nama'        => ['required', 'string', 'max:100'],
-            'email'       => ['required', 'email'],
             'k_group'     => ['required', 'integer', 'exists:m_group,k_group'],
             'kelamin'     => ['nullable', 'string', 'in:L,P'],
             'tmp_lahir'   => ['nullable', 'string', 'max:50'],
             'tgl_lahir'   => ['nullable', 'date_format:Y-m-d'],
-            'nip'         => ['nullable', 'string', 'max:20'],
+            'nik'         => ['nullable', 'string', 'max:16'],
+            'nip'         => ['nullable', 'string', 'max:30'],
             'no_telpon'   => ['nullable', 'numeric'],
             'no_hp'       => ['nullable', 'numeric'],
+            'k_propinsi'  => ['nullable', 'integer', 'exists:m_propinsi,k_propinsi'],
+            'k_kota'      => ['nullable', 'integer', 'exists:m_kota,k_kota'],
             'k_golongan'  => ['nullable', 'integer', 'exists:m_golongan,k_golongan'],
             'instansi_id' => ['nullable', 'sometimes', 'exists:instansi,instansi_id'],
         ];
