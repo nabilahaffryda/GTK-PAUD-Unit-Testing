d0259d28a59e28e00a0996da1d142dcc2827079f
Azhar Mashuri <azhar@jayantara.co.id>
2021-04-15 15:14:38 +0700

    Auto create data pengajar ketika menambahkan akun pengajar

--- backend/app/Http/Controllers/Instansi/Akun/PengajarController.php
+++ backend/app/Http/Controllers/Instansi/Akun/PengajarController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\Instansi\Akun;
 
 use App\Http\Controllers\Instansi\AkunController;
+use App\Http\Requests\Instansi\Admin\CreateRequest;
+use App\Http\Resources\BaseResource;
 use App\Models\MGroup;
+use App\Services\Instansi\PengajarService;
 
 class PengajarController extends AkunController
 {
     protected $kGroup = MGroup::PENGAJAR_DIKLAT_PAUD;
+
+    public function create(CreateRequest $request)
+    {
+        $this->validateGroup();
+
+        $params = array_merge($request->validated(), [
+            'k_group' => $this->kGroup,
+        ]);
+
+        $paudAdmin = $this->service->create(instansi(), $params);
+        app(PengajarService::class)->create($paudAdmin);
+        return BaseResource::make($paudAdmin);
+    }
 }
