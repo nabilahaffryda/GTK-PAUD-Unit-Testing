e8d8df14dcede2f4b17cfc83cf98baa7fe9ef8f8
Azhar Mashuri <azhar@jayantara.co.id>
2021-04-15 17:05:24 +0700

    Lepas get data master dari model

--- backend/app/Services/MasterService.php
+++ backend/app/Services/MasterService.php
@@ -2,8 +2,8 @@
 
 namespace App\Services;
 
-use Eloquent;
-use Str;
+use DB;
+use Illuminate\Database\QueryException;
 
 class MasterService
 {
@@ -19,34 +19,35 @@ class MasterService
             $field  = $fields[$index] ?? ['keterangan'];
             $filter = $filters[$index] ?? [];
 
-            /** @var Eloquent $class */
-            $class = '\\App\Models\\' . Str::camel($table);
-            if (!class_exists($class)) {
+            try {
+                $info = DB::select("SHOW KEYS FROM {$table} WHERE Key_name = 'PRIMARY'");
+            } catch (QueryException) {
                 $result[$index] = [];
                 continue;
             }
 
-            $key = (new $class())->getKeyName();
+            $key = $info[0]->Column_name;
 
-            $q = $class::select(array_unique(array_merge([$key], $field)));
+            $query = DB::table($table)
+                ->select(array_unique(array_merge([$key], $field)));
 
             if ($filter) {
                 foreach ($filter as $column => $value) {
                     if (is_array($value)) {
                         if ($value == array_values($value)) {
-                            $q->whereIn($column, $value);
+                            $query->whereIn($column, $value);
                         } elseif (isset($value['val'])) {
-                            $q->where($column, $value['op'] ?? '=', $value['val']);
+                            $query->where($column, $value['op'] ?? '=', $value['val']);
                         }
                     } else {
-                        $q->where($column, $value);
+                        $query->where($column, $value);
                     }
                 }
             } else {
-                $q->limit(1000);
+                $query->limit(1000);
             }
 
-            $res = $q->get();
+            $res = $query->get();
 
             if (count($field) == 1) {
                 $result[$index] = $res->pluck($field[0], $key);
