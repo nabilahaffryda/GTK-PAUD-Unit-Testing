00c4a8617b704657d2ba55e93f9dbb755c4822fb
Kukuh Ary Susanto <kukuh@jayantara.co.id>
2021-04-16 10:40:14 +0700

    Pasang acl akun pembimbing praktik

--- frontend/src/views/instansi/admin/list/pembimbing/actions.js
+++ frontend/src/views/instansi/admin/list/pembimbing/actions.js
@@ -1,5 +1,5 @@
 export default [
-  { icon: 'mdi-lock-reset', title: 'Reset Password', event: 'onReset', akses: 'pengajar.reset' },
-  { icon: 'mdi-pencil', title: 'Edit Akun', event: 'onEdit', akses: 'pengajar.update' },
-  { icon: 'mdi-delete', title: 'Hapus', event: 'onDelete', akses: 'pengajar.delete' },
+  { icon: 'mdi-lock-reset', title: 'Reset Password', event: 'onReset', akses: 'akun-pembimbing-praktik.reset' },
+  { icon: 'mdi-pencil', title: 'Edit Akun', event: 'onEdit', akses: 'akun-pembimbing-praktik.update' },
+  { icon: 'mdi-delete', title: 'Hapus', event: 'onDelete', akses: 'akun-pembimbing-praktik.delete' },
 ];
