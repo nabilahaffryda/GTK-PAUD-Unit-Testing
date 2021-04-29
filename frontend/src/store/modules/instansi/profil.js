df7943da6389e95d2a0310e28a48b359484c7101
Kukuh Ary Susanto <kukuh@jayantara.co.id>
2021-04-30 03:48:08 +0700

    fix: Penyesuaian url ajuan profil LPD

--- frontend/src/store/modules/instansi/profil.js
+++ frontend/src/store/modules/instansi/profil.js
@@ -36,12 +36,12 @@ export const actions = {
   },
   ajuan({ rootState }, payload) {
     const id = rootState.auth.instansi_id;
-    const url = `/i/${id}/${payload.jenis}/profil/${payload.id}/ajuan/create`;
+    const url = `/i/${id}/${payload.jenis}/${payload.jenis === 'lpd' ? '' : 'profil/'}${payload.id}/ajuan/create`;
     return http.post(url, payload.params).then(({ data }) => data);
   },
   batalAjuan({ rootState }, payload) {
     const id = rootState.auth.instansi_id;
-    const url = `/i/${id}/${payload.jenis}/profil/${payload.id}/ajuan/delete`;
+    const url = `/i/${id}/${payload.jenis}/${payload.jenis === 'lpd' ? '' : 'profil/'}${payload.id}/ajuan/delete`;
     return http.post(url, payload.params).then(({ data }) => data);
   },
 };
