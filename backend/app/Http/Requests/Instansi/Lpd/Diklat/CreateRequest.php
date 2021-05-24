538d724c62ffc8d9a13f0c49ff952dc7243c659d
villananda <villa@jayantara.co.id>
2021-05-24 15:22:20 +0700

    Form Request create dan index diklat

---
+++ backend/app/Http/Requests/Instansi/Lpd/Diklat/CreateRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests\Instansi\Lpd\Diklat;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nama'               => ['required', 'string'],
+            'singkatan'          => ['required', 'string'],
+            'deskripsi'          => ['nullable', 'string'],
+            'k_propinsi'         => ['required', 'integer', 'exists:m_propinsi,k_propinsi'],
+            'k_kota'             => ['required', 'integer', 'exists:m_kota,k_kota'],
+            'periode_diklat'     => ['required', 'string'],
+            'tgl_daftar_mulai'   => ['required', 'date_format:Y-m-d'],
+            'tgl_daftar_selesai' => ['required', 'date_format:Y-m-d'],
+        ];
+    }
+}
