600c0edc0ed6fc3ed18710d23f39c7072b0b0940
villananda <villa@jayantara.co.id>
2021-06-07 15:28:24 +0700

    Ubah method prosedur batal, kunci, dan batal-kunci verval

--- backend/routes/instansi.php
+++ backend/routes/instansi.php
@@ -46,9 +46,9 @@ Route::group(['middleware' => ['auth:akun', 'forcejson', 'valid.instansi', 'vali
         Route::get('lpd', [Lpd\VervalController::class, 'index']);
         Route::get('lpd/{paudInstansi}', [Lpd\VervalController::class, 'fetch']);
         Route::post('lpd/{paudInstansi}', [Lpd\VervalController::class, 'update']);
-        Route::post('lpd/{paudInstansi}/batal', [Lpd\Verval\BatalController::class, 'update']);
-        Route::post('lpd/{paudInstansi}/batal-kunci', [Lpd\Verval\KunciController::class, 'delete']);
-        Route::post('lpd/{paudInstansi}/kunci', [Lpd\Verval\KunciController::class, 'update']);
+        Route::get('lpd/{paudInstansi}/batal', [Lpd\Verval\BatalController::class, 'update']);
+        Route::get('lpd/{paudInstansi}/kunci', [Lpd\Verval\KunciController::class, 'update']);
+        Route::get('lpd/{paudInstansi}/batal-kunci', [Lpd\Verval\KunciController::class, 'delete']);
 
         Route::get('petugas', [Petugas\VervalController::class, 'index']);
         Route::get('petugas/{petugas}', [Petugas\VervalController::class, 'fetch']);
