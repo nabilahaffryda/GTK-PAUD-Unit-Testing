ed6de721df4f4aa32e4ed6ee1627fcf0bae8e445
Kukuh Ary Susanto <kukuh@jayantara.co.id>
2021-08-03 10:48:15 +0700

    Fitur Pindah Layanan di Diklat PAUD ref#5590

--- frontend/src/components/navbar/Topbar.vue
+++ frontend/src/components/navbar/Topbar.vue
@@ -4,6 +4,37 @@
     <v-toolbar-title v-html="title"></v-toolbar-title>
     <v-spacer></v-spacer>
     <template>
+      <v-divider vertical inset dark></v-divider>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <div v-bind="attrs" v-on="on">
+            <v-menu v-model="menu" left :close-on-content-click="false" offset-y :nudge-width="200">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn icon dark class="mx-2" v-bind="attrs" v-on="on">
+                  <v-icon>mdi-apps</v-icon>
+                </v-btn>
+              </template>
+
+              <v-card>
+                <v-list>
+                  <template v-for="(item, i) in layanans">
+                    <v-list-item :key="i" :href="item.url">
+                      <v-list-item-avatar size="16">
+                        <img src="/favicon.png" />
+                      </v-list-item-avatar>
+                      <v-list-item-content>
+                        <v-list-item-title class="caption" v-html="item.nama"></v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-divider :key="`div_${i}`"></v-divider>
+                  </template>
+                </v-list>
+              </v-card>
+            </v-menu>
+          </div>
+        </template>
+        <span>Program Lain</span>
+      </v-tooltip>
       <v-list color="transparent">
         <v-list-item @click="toggleR" class="pa-0 ma-0">
           <v-list-item-avatar :size="32" color="grey lighten-4" class="ma-0 mr-1">
