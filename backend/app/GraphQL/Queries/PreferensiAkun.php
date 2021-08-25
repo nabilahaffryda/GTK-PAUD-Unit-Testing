f19fef9c3c27c8a9f8a957611635a0867cfda6b8
Azhar Mashuri <azhar@jayantara.co.id>
2021-08-25 11:14:17 +0700

    Update query class untuk preferensi akun

--- backend/app/GraphQL/Queries/PreferensiAkun.php
+++ backend/app/GraphQL/Queries/PreferensiAkun.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
-class Preferensi
+class PreferensiAkun
 {
     protected Instansi $instansi;
     /**
