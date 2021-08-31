a069a85071ce4f9572796f2dba4c483290f17bed
Azhar Mashuri <azhar@jayantara.co.id>
2021-08-31 10:00:41 +0700

    Tambah master konfirmasi untuk peserta kelas

--- backend/app/Http/Controllers/Instansi/Kelas/VervalController.php
+++ backend/app/Http/Controllers/Instansi/Kelas/VervalController.php
@@ -60,6 +60,7 @@ class VervalController extends Controller
         $q = $kelas->paudKelasPesertas()
             ->with([
                 'ptk',
+                'mKonfirmasiPaud',
             ]);
 
         return BaseCollection::make($q->paginate((int)$request->get('count', 10)));
