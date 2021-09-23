db5523b5f65c7094f8185d82007b7b68adfc49c5
Azhar Mashuri <azhar@jayantara.co.id>
2021-09-23 10:23:00 +0700

    #6851 Kelas diklat tidak muncul pada admin kelas yang telah ditambahkan pada kelas https://lapor.siap.id/issues/6851

--- backend/app/Services/Instansi/PetugasKelasService.php
+++ backend/app/Services/Instansi/PetugasKelasService.php
@@ -4,6 +4,7 @@ namespace App\Services\Instansi;
 
 use App\Exceptions\FlowException;
 use App\Models\MKonfirmasiPaud;
+use App\Models\MPetugasPaud;
 use App\Models\MVervalPaud;
 use App\Models\PaudKelas;
 use App\Models\PaudKelasPetugas;
@@ -58,10 +59,13 @@ class PetugasKelasService
             ->whereHas('paudKelasPetugases', function ($query) use ($akunId) {
                 $query
                     ->join('paud_petugas', 'paud_petugas.paud_petugas_id', '=', 'paud_kelas_petugas.paud_petugas_id')
-                    ->where([
-                        'paud_petugas.akun_id'                 => $akunId,
-                        'paud_kelas_petugas.k_konfirmasi_paud' => MKonfirmasiPaud::BERSEDIA,
-                    ]);
+                    ->where('paud_petugas.akun_id', '=', $akunId)
+                    ->where(function ($query) {
+                        $query->orWhere([
+                            'paud_kelas_petugas.k_konfirmasi_paud' => MKonfirmasiPaud::BERSEDIA,
+                            'paud_kelas_petugas.k_petugas_paud'    => MPetugasPaud::ADMIN_KELAS,
+                        ]);
+                    });
             })
             ->when($args['keyword'] ?? null, function ($query, $value) {
                 $query->where('nama', 'like', '%' . $value . '%');
