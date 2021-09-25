4a7e94fdd59bd5458e8b99e93cfc976683ece2b8
villananda <villa@jayantara.co.id>
2021-09-25 22:33:12 +0700

    #6946: Selain Admin Kelas, daftar kandidat petugas hanya yang sudah lulus bimtek

--- backend/app/Services/Instansi/KelasService.php
+++ backend/app/Services/Instansi/KelasService.php
@@ -182,8 +182,10 @@ class KelasService
             ->when($params['k_petugas_paud'] != MPetugasPaud::PENGAJAR, function ($query) use ($paudDiklat) {
                 $query->where('paud_petugas.instansi_id', '=', $paudDiklat->instansi_id);
             })
+            ->when($params['k_petugas_paud'] != MPetugasPaud::ADMIN_KELAS, function ($query) {
+                $query->where('paud_petugas.is_refreshment', '=', 1);
+            })
             ->where('paud_petugas.k_petugas_paud', '=', $params['k_petugas_paud'])
-            ->where('paud_petugas.is_refreshment', '=', 1)
             ->whereNotExists(function ($query) use ($params) {
                 $query->select(DB::raw(1))
                     ->from('paud_kelas_petugas')
