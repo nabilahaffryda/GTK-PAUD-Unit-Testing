b7f5ebabfce6292351bdc21ddd26edf6e098b176
Azhar Mashuri <azhar@jayantara.co.id>
2021-09-29 14:25:49 +0700

    Validasi data kota dan propinsi yang dikirim sesuai dengan level lpd

--- backend/app/Services/Instansi/DiklatService.php
+++ backend/app/Services/Instansi/DiklatService.php
@@ -6,6 +6,8 @@ namespace App\Services\Instansi;
 
 use App\Exceptions\SaveException;
 use App\Models\Instansi;
+use App\Models\MKota;
+use App\Models\MLpdPaud;
 use App\Models\PaudDiklat;
 use App\Models\PaudInstansi;
 use Arr;
@@ -30,19 +32,37 @@ class DiklatService
         return $query;
     }
 
+    /**
+     * @throws SaveException
+     */
     public function create(Instansi $instansi, array $params)
     {
+        /** @var PaudInstansi $paudInstansi */
         $paudInstansi = PaudInstansi::where([
             'instansi_id' => $instansi->instansi_id,
             'tahun'       => config('paud.tahun'),
             'angkatan'    => config('paud.angkatan'),
-        ])->first();
+        ])->firstOrFail();
 
         $paudDiklat                   = new PaudDiklat($params);
         $paudDiklat->tahun            = config('paud.tahun');
         $paudDiklat->angkatan         = config('paud.angkatan');
         $paudDiklat->instansi_id      = $instansi->instansi_id;
         $paudDiklat->paud_instansi_id = $paudInstansi->paud_instansi_id;
+
+        switch ($paudInstansi->k_lpd_paud) {
+            default:
+            case MLpdPaud::LPD_KOTA_KABUPATEN:
+                $paudDiklat->k_kota = $instansi->k_kota;
+            case MLpdPaud::LPD_PROVINSI:
+                $paudDiklat->k_propinsi = $instansi->k_propinsi;
+            case MLpdPaud::LPD_PUSAT:
+        }
+
+        $mKota = MKota::findOrFail($paudDiklat->k_kota);
+        if ($mKota->k_propinsi != $paudDiklat->k_propinsi) {
+            throw new SaveException('Kota dan propinsi yang dipilih tidak sesuai');
+        }
 
         $paudDiklat->save();
 
