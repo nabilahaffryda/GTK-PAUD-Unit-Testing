113cdc8757d1bbfba5ca816f4d4512e3d4c350df
Azhar Mashuri <azhar@jayantara.co.id>
2021-10-03 06:56:07 +0700

    Mark jika sync akan throw exception

--- backend/app/Jobs/SyncKelasJob.php
+++ backend/app/Jobs/SyncKelasJob.php
@@ -4,6 +4,7 @@ namespace App\Jobs;
 
 use App\Models\PaudKelas;
 use App\Services\Instansi\KelasService;
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -41,6 +42,7 @@ class SyncKelasJob implements ShouldQueue
      * Execute the job.
      *
      * @return void
+     * @throws Exception
      */
     public function handle()
     {
