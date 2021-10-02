83db726c543eb975314a91da3f17467c1a387654
Azhar Mashuri <azhar@jayantara.co.id>
2021-10-02 22:23:17 +0700

    Tambah konfigurasi elearning

--- backend/config/paud.php
+++ backend/config/paud.php
@@ -3,4 +3,21 @@
 return [
     'tahun'    => 2021,
     'angkatan' => 1,
+
+    'elearning' => [
+        'sync' => true,
+
+        'instansi-id'     => [1, 2],
+        'diklat-id'       => [1, 2],
+        'jenis-diklat-id' => [1, 1],
+
+        'group-id-petugas' => [
+            1 => 4, // pengajar
+            2 => 6, // pengajar tambahan
+            3 => 7, // pembimbing praktik
+            4 => 5, // admin kelas
+        ],
+
+        'group-id-peserta' => 8,
+    ],
 ];
