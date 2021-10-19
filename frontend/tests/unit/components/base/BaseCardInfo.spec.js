2a9e987e2ea8c564ef222ddce001f255e6ad0179
nabilahaffyda <nabilahaffryda88@gmail.com>
2021-10-19 09:28:14 +0700

    fix error on BaseCardInfo

--- frontend/tests/unit/components/base/BaseCardInfo.spec.js
+++ frontend/tests/unit/components/base/BaseCardInfo.spec.js
@@ -1,24 +1,30 @@
 import { mount, createLocalVue } from "@vue/test-utils";
 import BaseCardInfo from "@/components/base/BaseCardInfo.vue";
 import Vuetify from 'vuetify';
+import Vue from 'vue';
+
+Vue.use(Vuetify)
+const localVue = createLocalVue();
 
 describe('BaseCardInfo.vue', () => {
+    let vuetify
+    beforeEach(() => {
+        vuetify = new Vuetify()
+    })
     test('should show card info', () => {
         const VCard = {
             props: ['size', 'icon', 'title', 'info'],
             template: '<div><slot :size="size" /></div>'
         }
-        const localVue = createLocalVue();
-        localVue.use(Vuetify)
-        localVue.use(BaseCardInfo)
 
         const wrapper = mount(BaseCardInfo, {
-            localVue: localVue,
+            localVue,
+            vuetify,
             stubs: {
                 VCard
             }
         });
-
+        expect(wrapper).toMatchSnapshot();
         // mount(BaseCardInfo, {
         //     stubs: {
         //         VCard
