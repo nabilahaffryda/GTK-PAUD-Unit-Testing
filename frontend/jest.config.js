6f9a342abe727a48200a7c6b81d224a1435c03c2
nabilahaffyda <nabilahaffryda88@gmail.com>
2021-10-21 13:19:37 +0700

    add dependencies and settings in config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -16,7 +16,12 @@ module.exports = {
     "^components/(.*)": "<rootDir>/src/components/$1",
     'assets/(.*)': '<rootDir>/src/assets',
     '^@/(.*)$': '<rootDir>/src/$1',
-    '^~/(.*)$': '<rootDir>/$1'
+    '^~/(.*)$': '<rootDir>/$1',
+    'vuetify/lib(.*)': '<rootDir>/node_modules/vuetify/es5$1',
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy"
   },
-  transformIgnorePatterns: ['/node_modules/(?!lib-to-transform|other-lib)']
+  transformIgnorePatterns: [
+    '/node_modules/(?!lib-to-transform|other-lib)',
+    '<rootDir>/node_modules/(?!(vuetify)/)']
 }
