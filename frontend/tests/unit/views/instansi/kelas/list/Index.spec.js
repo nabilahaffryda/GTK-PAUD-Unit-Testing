1a0e848d8c349e602e23e1a53eab2fee3c6a5074
nabilahaffyda <nabilahaffryda88@gmail.com>
2021-11-08 16:22:19 +0700

    unit test on views/instansi/kelas/list/Index

---
+++ frontend/tests/unit/views/instansi/kelas/list/Index.spec.js
@@ -0,0 +1,215 @@
+import { createLocalVue, mount } from "@vue/test-utils";
+import Vuetify from 'vuetify';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Vuex from "vuex";
+import axios from 'axios';
+import Index from "@/views/instansi/kelas/list/Index.vue";
+import { getDeepObj, arrayToObject, isObject, duration } from '@utils/format';
+
+Vue.use(Vuetify)
+const localVue = createLocalVue();
+
+localVue.use(VueRouter)
+const router = new VueRouter();
+
+localVue.use(Vuex)
+const store = new Vuex.Store({
+    modules: {
+        master: {
+            namespaced: true,
+            actions: {
+                getMasters() {
+                    return true
+                },
+            }
+        },
+        petugasKelas: {
+            namespaced: true,
+            actions: {
+                fetch() {
+                    return true
+                },
+                getDetail() {
+                    return true
+                }
+            }
+        }
+    },
+})
+localVue.mixin({
+    methods: {
+        $getDeepObj(obj, desc) {
+            return getDeepObj(obj, desc);
+        },
+        $arrToObj(array, key) {
+            if (!array || !array.length) return {};
+            return arrayToObject(array, key);
+        },
+        $allow(akses, policy) {
+            // cek jika tidak ada akses
+            if (!akses || (akses && typeof akses === 'boolean')) return typeof akses === 'boolean' ? akses : true;
+
+            // jika ada ambil preferensi akses
+            const akseses = store.getters['preferensi/akseses'] || [];
+
+            let allow = false;
+            const pathAkses = akses && typeof akses === 'string' && akses.split('|');
+            if (pathAkses.length) {
+                pathAkses.forEach((path) => {
+                    if (allow) return;
+                    if (!allow && policy) {
+                        allow =
+                            !path || path === 'true' || ((akseses || []).filter((item) => item === path).length > 0 && policy[path]);
+                    } else if (!allow && !policy) {
+                        allow = !path || path === 'true' || (akseses || []).filter((item) => item === path).length > 0;
+                    }
+                });
+            }
+
+            return allow;
+        },
+        $durasi(start, end, options) {
+            return duration(start, end, options);
+        },
+        $isObject(data) {
+            return isObject(data || {});
+        },
+    }
+})
+jest.mock('axios');
+describe('Index.vue', () => {
+    let vuetify;
+    beforeEach(() => {
+        vuetify = new Vuetify()
+        const responseGet = {
+            data: {
+                policies: {
+                    akses: true
+                }
+            },
+        }
+        axios.get.mockResolvedValue(responseGet);
+    })
+    afterEach(() => {
+        jest.resetModules()
+        jest.clearAllMocks()
+    })
+    function wrapperFactory({ } = {}) {
+        return mount(Index, {
+            localVue,
+            vuetify,
+            router,
+            store,
+            stubs: {
+                DetailKelas: true,
+                BaseModalFull: true,
+                BaseBreadcrumbs: true,
+            },
+            data() {
+                return {
+                    Datatables: false,
+                    reload: false,
+                    actions: [
+                        {
+                            icon: 'mdi-information',
+                            title: 'Info Kelas',
+                            event: 'onDetail',
+                            akses: true
+                        }
+                    ]
+                }
+            }
+        })
+    }
+    test('call allow method', () => {
+        const wrapper = wrapperFactory();
+        wrapper.vm.allow = jest.fn();
+        wrapper.vm.allow();
+        expect(wrapper.vm.allow.mock.calls.length).toBe(1);
+    })
+
+    test('call toLms method', () => {
+        const wrapper = wrapperFactory();
+        wrapper.vm.toLms = jest.fn();
+        wrapper.vm.toLms();
+        expect(wrapper.vm.toLms.mock.calls.length).toBe(1);
+    })
+
+    test('render list table', async () => {
+        const wrapper = wrapperFactory();
+        wrapper.setData({ Datatables: true });
+        await wrapper.vm.$nextTick();
+        expect(wrapper.find('[data-testid="list-table"]').exists()).toBe(true);
+    })
+    test('calls onReload when button is clicked', async () => {
+        const wrapper = wrapperFactory();
+        wrapper.setData({ reload: true });
+        wrapper.vm.onReload = jest.fn();
+        wrapper.vm.onReload();
+        await wrapper.vm.$nextTick();
+        expect(wrapper.find('.mdi-reload').exists()).toBe(true);
+        const btn = wrapper.find('.mdi-reload')
+        btn.trigger('click')
+        expect(wrapper.vm.onReload.mock.calls.length).toBe(1);
+    })
+    test('test total data from data tables ', async () => {
+        const wrapper = wrapperFactory();
+        wrapper.find('[data-testid="list-table"]')
+        wrapper.setData({ total: 2 });
+        await wrapper.vm.$nextTick();
+        expect(wrapper.vm.total).toBe(2)
+    })
+    test('Enter search text and check the value of keyword', async () => {
+        const wrapper = wrapperFactory();
+        wrapper.find('[data-testid="search"]')
+        wrapper.setData({ keyword: 'Kelas A' })
+        await wrapper.vm.$nextTick();
+        expect(wrapper.vm.keyword).toBe('Kelas A')
+    })
+    test('click search button and clear input', () => {
+        const wrapper = wrapperFactory();
+        wrapper.find('.mdi-magnify').trigger("click");
+        const textInput = wrapper.find('[data-testid="search"]')
+        expect(textInput.text()).toMatch('')
+        expect(wrapper.vm.keyword).toBe('')
+    })
+    test('call onAction when button is clicked', async () => {
+        const wrapper = wrapperFactory();
+        wrapper.vm.onAction = jest.fn()
+        wrapper.vm.onAction()
+        wrapper.setData({
+            data: [
+                {
+                    nama: "Kelas A",
+                    jml_pengajar: 4,
+                    id: 2,
+                    deskripsi: "Lorem Ipsum lah",
+                    paud_diklat: {
+                        data: {
+                            paud_periode: {
+                                data: {
+                                    tgl_diklat_mulai: "2021-02-15",
+                                    tgl_diklat_selesai: "2021-02-26"
+                                }
+                            }
+                        }
+                    }
+                }
+            ],
+            total: 1,
+            actions: [
+                {
+                    icon: 'mdi-information',
+                    title: 'Info Kelas',
+                    event: 'onDetail',
+                    akses: true
+                }
+            ]
+        });
+        await wrapper.vm.$nextTick()
+        expect(wrapper.find('.mdi-dots-vertical').exists()).toBe(true);
+        const button = wrapper.find('.mdi-dots-vertical')
+        button.trigger('click')
+    })
+})
