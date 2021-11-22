b1b5555d0262bfbf5dbcc5fbbc5b78743f6e5554
Fahmiko Purnama Putra <pp.fahmiko@gmail.com>
2021-11-22 16:36:40 +0700

    feat: Aksi daftar kelas diklat luring

--- frontend/src/store/modules/instansi/petugas.kelas.js
+++ frontend/src/store/modules/instansi/petugas.kelas.js
@@ -2,28 +2,43 @@
 import kitsu from '@plugins/kitsu';
 let $ajax;
 
+export const state = {
+  jenis: null,
+};
+
+export const mutations = {
+  SET_JENIS(state, newValue) {
+    state.jenis = newValue;
+  },
+};
+
 export const actions = {
-  async fetch({ rootState }, payload) {
+  async fetch({ rootState, commit }, payload) {
+    const jenis = {
+      daring: '',
+      luring: '-luring',
+    };
     const id = rootState.auth.instansi_id;
     $ajax = kitsu({
-      baseURL: process.env.VUE_APP_API_URL + `/i/${id}/petugas/kelas`,
+      baseURL: process.env.VUE_APP_API_URL + `/i/${id}/petugas${jenis[payload.attr.jenis]}/kelas`,
     });
+    commit('SET_JENIS', jenis[payload.attr.jenis]);
     return await $ajax.get('/', { params: payload.params });
   },
 
-  async getDetail({ rootState }, payload) {
+  async getDetail({ rootState, state }, payload) {
     const id = rootState.auth.instansi_id;
     $ajax = kitsu({
-      baseURL: process.env.VUE_APP_API_URL + `/i/${id}/petugas/kelas`,
+      baseURL: process.env.VUE_APP_API_URL + `/i/${id}/petugas${state.jenis}/kelas`,
     });
     return await $ajax.get(`/${payload.id}`);
   },
 
-  async getListKelas({ rootState }, payload) {
+  async getListKelas({ rootState, state }, payload) {
     const id = rootState.auth.instansi_id;
     const params = Object.assign({}, payload.params || {}, { page: payload.page || 1 });
     $ajax = kitsu({
-      baseURL: process.env.VUE_APP_API_URL + `/i/${id}/petugas/kelas/${payload.id}/peserta`,
+      baseURL: process.env.VUE_APP_API_URL + `/i/${id}/petugas${state.jenis}/kelas/${payload.id}/peserta`,
     });
     return await $ajax.get(`/`, { params });
   },
