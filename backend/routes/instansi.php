84608348f9c0e69efd8fd1e3abddb7fd675c2987
Azhar Mashuri <azhar@jayantara.co.id>
2021-11-26 10:54:11 +0700

    fix: Routing delete menggunakan get

--- backend/routes/instansi.php
+++ backend/routes/instansi.php
@@ -103,7 +103,7 @@ Route::group(['middleware' => ['auth:akun', 'forcejson', 'valid.instansi', 'vali
             Route::get('kelas/{kelas}/petugas', [Lpd\Kelas\PetugasController::class, 'index']);
             Route::get('kelas/{kelas}/petugas/kandidat', [Lpd\Kelas\PetugasKandidatController::class, 'index']);
             Route::post('kelas/{kelas}/petugas/create', [Lpd\Kelas\PetugasController::class, 'create']);
-            Route::get('kelas/{kelas}/petugas/{petugas}/delete', [Lpd\Kelas\PetugasController::class, 'delete']);
+            Route::post('kelas/{kelas}/petugas/{petugas}/delete', [Lpd\Kelas\PetugasController::class, 'delete']);
         });
     });
 
