b6d07a3554823594c6fc57f71680d14fcfec19fc
Azhar Mashuri <azhar@jayantara.co.id>
2021-12-03 15:41:24 +0700

    Selalu call generate pdf

--- backend/app/GraphQL/Mutations/Ptk/KelasPeserta.php
+++ backend/app/GraphQL/Mutations/Ptk/KelasPeserta.php
@@ -144,69 +144,68 @@ class KelasPeserta
             throw new FlowException('Survey belum diisi');
         }
 
-        if (!$kelasPeserta->url_download) {
+        $ptk          = ptk();
+        $diklat       = $kelasPeserta->paudKelas->paudDiklat;
+        $periode      = $diklat->paudPeriode;
+        $paudInstansi = $diklat->paudInstansi;
+
+        $berkases = $paudInstansi->paudInstansiBerkases->keyBy('k_berkas_lpd_paud');
+        if (!$berkases->has([8, 9, 10])) {
+            throw new FlowException('Berkas LPD belum lengkap');
+        }
+
+        $instansi = $diklat->instansi;
+
+        /** @var Sekolah $sekolah */
+        $sekolah = $ptk->ptkSekolahs->first()?->sekolah;
+
+        $params = [
+            'k_sertifikat' => '213',
+            'angkatan'     => '1',
+            'user_id'      => $ptk->ptk_id,
+            'model_id'     => $kelasPeserta->paud_kelas_peserta_id,
+
+            'peran'       => 'Peserta',
+            'nama'        => $ptk->nama,
+            'nomor_surat' => '2611/B4/GT.00.04/2021',
+            'instansi'    => $sekolah->nama,
+
+            'tgl_mulai'   => $periode->tgl_diklat_mulai?->toDateString(),
+            'tgl_selesai' => $periode->tgl_diklat_selesai?->toDateString(),
+            'tgl_cetak'   => $periode->tgl_diklat_selesai?->toDateString(),
+            'predikat'    => strtoupper($kelasPeserta->predikat),
+
+            'data' => [
+                'lpd_nama'     => $instansi->nama,
+                'lpd_pimpinan' => $paudInstansi->nama_penanggung_jawab,
+                'lpd_lokasi'   => $instansi->mKota->keterangan,
+
+                'kota'     => $sekolah?->mKota?->keterangan,
+                'propinsi' => $sekolah?->mPropinsi?->keterangan,
+                'lokasi'   => $diklat->mKota->keterangan . ', Provinsi ' . $diklat->mPropinsi->keterangan,
+
+                'diklat_jenis'   => 'Diklat Berjenjang ',
+                'diklat_jenjang' => 'Tingkat Dasar',
+                'diklat_moda'    => 'Moda Daring Kombinasi',
+
+                'lpd_url_logo'    => $berkases[8]->url,
+                'lpd_url_ttd'     => $berkases[9]->url,
+                'lpd_url_stempel' => $berkases[10]->url,
+            ],
+        ];
+
+        $remote = new SertifikatRemote();
+        $resp   = $remote->create($params);
+
+        $url = $resp['sertifikat']['url_unduh'] ?? '';
+        if (!$url) {
+            $nomor = $resp['sertifikat']['nomor'];
+            $resp  = $remote->search(['nomor' => $nomor]);
+            $url   = $resp['data'][0]['url_unduh'] ?? '';
+        }
+
+        if ($url != $kelasPeserta->url_download) {
             $kelasPeserta->wkt_download = Carbon::now();
-
-            $ptk          = ptk();
-            $diklat       = $kelasPeserta->paudKelas->paudDiklat;
-            $periode      = $diklat->paudPeriode;
-            $paudInstansi = $diklat->paudInstansi;
-
-            $berkases = $paudInstansi->paudInstansiBerkases->keyBy('k_berkas_lpd_paud');
-            if (!$berkases->has([8, 9, 10])) {
-                throw new FlowException('Berkas LPD belum lengkap');
-            }
-
-            $instansi = $diklat->instansi;
-
-            /** @var Sekolah $sekolah */
-            $sekolah = $ptk->ptkSekolahs->first()?->sekolah;
-
-            $params = [
-                'k_sertifikat' => '213',
-                'angkatan'     => '1',
-                'user_id'      => $ptk->ptk_id,
-                'model_id'     => $kelasPeserta->paud_kelas_peserta_id,
-
-                'peran'       => 'Peserta',
-                'nama'        => $ptk->nama,
-                'nomor_surat' => '2611/B4/GT.00.04/2021',
-                'instansi'    => $sekolah->nama,
-
-                'tgl_mulai'   => $periode->tgl_diklat_mulai?->toDateString(),
-                'tgl_selesai' => $periode->tgl_diklat_selesai?->toDateString(),
-                'tgl_cetak'   => $periode->tgl_diklat_selesai?->toDateString(),
-                'predikat'    => strtoupper($kelasPeserta->predikat),
-
-                'data' => [
-                    'lpd_nama'     => $instansi->nama,
-                    'lpd_pimpinan' => $paudInstansi->nama_penanggung_jawab,
-                    'lpd_lokasi'   => $instansi->mKota->keterangan,
-
-                    'kota'     => $sekolah?->mKota?->keterangan,
-                    'propinsi' => $sekolah?->mPropinsi?->keterangan,
-                    'lokasi'   => $diklat->mKota->keterangan . ', Provinsi ' . $diklat->mPropinsi->keterangan,
-
-                    'diklat_jenis'   => 'Diklat Berjenjang ',
-                    'diklat_jenjang' => 'Tingkat Dasar',
-                    'diklat_moda'    => 'Moda Daring Kombinasi',
-
-                    'lpd_url_logo'    => $berkases[8]->url,
-                    'lpd_url_ttd'     => $berkases[9]->url,
-                    'lpd_url_stempel' => $berkases[10]->url,
-                ],
-            ];
-
-            $remote = new SertifikatRemote();
-            $resp   = $remote->create($params);
-
-            $url = $resp['sertifikat']['url_unduh'] ?? '';
-            if (!$url) {
-                $nomor = $resp['sertifikat']['nomor'];
-                $resp  = $remote->search(['nomor' => $nomor]);
-                $url   = $resp['data'][0]['url_unduh'] ?? '';
-            }
-
             $kelasPeserta->url_download = $url;
             $kelasPeserta->save();
         }
