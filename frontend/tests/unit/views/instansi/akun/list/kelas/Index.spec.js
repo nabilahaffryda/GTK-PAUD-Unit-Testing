e9330370e47d5ea5ed9f182f0ab04744bcebdb0b
nabilahaffyda <nabilahaffryda88@gmail.com>
2021-12-22 09:10:36 +0700

    Unit test on views/instansi/akun/list/kelas/Index

---
+++ frontend/tests/unit/views/instansi/akun/list/kelas/Index.spec.js
@@ -0,0 +1,142 @@
+import { createLocalVue, mount, } from "@vue/test-utils";
+import Vuetify from 'vuetify';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Vuex from "vuex";
+import Index from "@/views/instansi/akun/list/kelas/Index.vue";
+import { getDeepObj, isObject, assetsUrl, localDate } from '@utils/format';
+
+Vue.use(Vuetify)
+const localVue = createLocalVue();
+
+document.body.setAttribute('data-app', true)
+
+localVue.use(VueRouter)
+const router = new VueRouter();
+
+localVue.use(Vuex)
+const store = new Vuex.Store({
+    modules: {
+        master: {
+            namespaced: true,
+            actions: {
+                getMasters() {
+                    return true
+                },
+            }
+        },
+        institusi: {
+            namespaced: true,
+            listInstansis: {
+                fetch() {
+                    return true
+                }
+            }
+        },
+        akun: {
+            namespaced: true,
+            actions: {
+                fetch() {
+                    return true
+                },
+                create() {
+                    return true
+                },
+                update() {
+                    return true
+                },
+                listGroups() {
+                    return true
+                },
+                action() {
+                    return true
+                },
+                lookup() {
+                    return true
+                },
+                getDetail() {
+                    return true
+                },
+                downloadList() {
+                    return true
+                },
+                templateUpload() {
+                    return true
+                },
+                upload() {
+                    return true
+                },
+                setStatus() {
+                    return true
+                },
+            }
+        }
+    },
+})
+
+localVue.mixin({
+    methods: {
+        $getDeepObj(obj, desc) {
+            return getDeepObj(obj, desc);
+        },
+        $allow() {
+            return true;
+        },
+        $isObject(data) {
+            return isObject(data || {});
+        },
+        $imgUrl: (url) => (url ? assetsUrl(url) : ''),
+        $localDate(date, short, withTime, usingNumber, useSeconds) {
+            if (!date) return '-';
+            return localDate(date, short, withTime, usingNumber, useSeconds);
+        },
+    }
+})
+
+describe('Index.vue', () => {
+    let vuetify;
+    beforeEach(() => {
+        vuetify = new Vuetify()
+    })
+    function wrapperFactory({ } = {}) {
+        return mount(Index, {
+            localVue,
+            vuetify,
+            router,
+            store,
+            stubs: {
+                BaseModalFull: true,
+            },
+        })
+    }
+
+    test('test search button', () => {
+        const wrapper = wrapperFactory()
+        wrapper.vm.onSearch = jest.fn();
+        expect(wrapper.find('.mdi-magnify').exists()).toBe(true)
+        wrapper.vm.$nextTick()
+        const search = wrapper.find('.mdi-magnify')
+        search.trigger('click')
+        wrapper.vm.onSearch()
+    })
+
+    test('test reload button', () => {
+        const wrapper = wrapperFactory();
+        wrapper.vm.onReload = jest.fn();
+        wrapper.vm.$nextTick();
+        expect(wrapper.find('.mdi-reload').exists()).toBe(true);
+        const btn = wrapper.find('.mdi-reload')
+        btn.trigger('click')
+        wrapper.vm.onReload();
+    })
+
+    test('test filter button', () => {
+        const wrapper = wrapperFactory();
+        wrapper.vm.onFilter = jest.fn();
+        wrapper.vm.$nextTick();
+        expect(wrapper.find('.mdi-filter-variant').exists()).toBe(true);
+        const button = wrapper.find('.mdi-filter-variant')
+        button.trigger('click')
+        wrapper.vm.onFilter();
+    })
+})
