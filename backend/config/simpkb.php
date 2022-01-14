b2c3f51b3447e0f6d0c5bdc4881d5eb68d30fe1b
nabilahaffyda <nabilahaffryda88@gmail.com>
2022-01-14 17:04:10 +0700

    delete backend side

--- backend/config/simpkb.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'url' => env('SIMPKB_URL', 'https://app-dev.simpkb.id'),
-
-    'sertifikat' => [
-        'validasi-url' => env('SERTIFIKAT_VALIDASI_URL', 'https://sertifikat.simpkb.id/%s'),
-    ],
-];
